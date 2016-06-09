3c06ecb1ca6f37fdba8a9780cb0af99b762ff773
DarkNik <mitakidisnikos@gmail.com>
2016-06-09 16:37:54 +0300

    diorthsois ston  memmori managment kai sto pkmObject prosthiki get set ivs

--- PkmEditor/pkmObject/index.js
+++ PkmEditor/pkmObject/index.js
@@ -1,25 +1,65 @@
-var memmory = require('./../memmory');
+var Memory = require('./../memory');
 
 class pkmObject {
     constructor(binary, type) {
         this._bin = binary;
-        this._type = type | 'pkx';
+        this._type = type || 'pkx';
+    }
+
+    private setIVs(IVs, isEgg, isNicknamed) {
+        let offset = Memory[this._type].INDIVIDUAL_VALUES;
+        let current_data = this.GetIndividualValues();
+        let IV32 = new Uint32Array(1);
+        let IV_HP = IVs.hp || current_data.hp;
+        let IV_Attack = IVs.attack || current_data.attack;
+        let IV_Defense = IVs.defense || current_data.defense;
+        let IV_Speed = IVs.speed || current_data.speed;
+        let IV_SpecialAttack = IVs.specialattack || current_data.specialattack;
+        let IV_SpecialDefense = IVs.specialdefense || current_data.specialdefense;
+        let Is_Egg = Number(isEgg) || current_data.is_egg;
+        let Is_Nicknamed = Number(isNicknamed) || current_data.is_nicknamed;
+        IV32[0] = IV_HP & 0x1F;
+        IV32[0] |= ((IV_Attack & 0x1F) << 5);
+        IV32[0] |= ((IV_Defense & 0x1F) << 10);
+        IV32[0] |= ((IV_Speed & 0x1F) << 15);
+        IV32[0] |= ((IV_SpecialAttack & 0x1F) << 20);
+        IV32[0] |= ((IV_SpecialDefense & 0x1F) << 25);
+        IV32[0] |= (Is_Egg << 30);
+        IV32[0] |= (Is_Nicknamed << 31);
+        let u8a = new Uint8Array(IV32.buffer);
+        Memory.RW.setValueAt(offset.address, u8a, offset.bits, this._bin);
+    }
+
+    private getIVs() {
+        let offset = Memory[this._type].map.INDIVIDUAL_VALUES;
+        let memory = Memory.RW.getValueAt(offset.address, offset.bits, this._bin);
+        let u32a = new Uint32Array(memory.buffer);
+        let ivs = {};
+        ivs.hp = (u32a[0] & 0x1F);
+        ivs.attack = (u32a[0] >> 5) & 0x1F;
+        ivs.defense = (u32a[0] >> 10) & 0x1F;
+        ivs.speed = (u32a[0] >> 15) & 0x1F;
+        ivs.specialattack = (u32a[0] >> 20) & 0x1F;
+        ivs.specialdefense = (u32a[0] >> 25) & 0x1F;
+        ivs.is_egg = ((u32a[0] >> 30) & 1);
+        ivs.is_nicknamed = ((u32a[0] >> 31));
+        return ivs;
     }
     
     get nationalID() {
-        let offset = memmory[this._type].map.NATIONAL_POKEDEX_ID;
+        let offset = Memory[this._type].map.NATIONAL_POKEDEX_ID;
         if (offset) {
-            let value = memmory.RW.getValueAt(offset.address, offset.bits, this._bin);
-            let u16a = new Uint16Array(value.buffer);
+            let memory = Memory.RW.getValueAt(offset.address, offset.bits, this._bin);
+            let u16a = new Uint16Array(memory.buffer);
             return u16a[0];
         } else return null;
     }
     set nationalID(pokedexId) {
-        let offset = memmory[this._type].map.NATIONAL_POKEDEX_ID;
+        let offset = Memory[this._type].map.NATIONAL_POKEDEX_ID;
         if (offset) {
             let buffer = new Uint16Array([pokedexId]);
-            let value = new Uint8Array(buffer.buffer);
-            memmory.RW.setValueAt(offset.address, value, offset.bits, this._bin);
+            let memory = new Uint8Array(buffer.buffer);
+            Memory.RW.setValueAt(offset.address, memory, offset.bits, this._bin);
         } else return false;
         return true
 
