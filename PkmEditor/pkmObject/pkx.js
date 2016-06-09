ed3ad9861719e0e99bd9f42b1496b4558bea3f0c
DarkNik <mitakidisnikos@gmail.com>
2016-06-09 18:49:17 +0300

    get set nickName

--- PkmEditor/pkmObject/pkx.js
+++ PkmEditor/pkmObject/pkx.js
@@ -99,6 +99,21 @@ class PKX {
         let memory = new Uint8Array([u8a[0], u8a[1], u8a[2]]);
         Memory.RW.setValueAt(offset.address, memory, offset.bits, this._bin);
     }
+    
+    get nickName() {
+        let offset = Memory.pkx.map.NICKNAME;
+        let memory = Memory.RW.getValueAt(offset.address, offset.bits, this._bin);
+        return String.fromCharCode.apply(null, memory);
+    }
+    set nickName(nickname) {
+        let offset = Memory.pkx.map.NICKNAME;
+        let u16a = new Uint16Array(0x9);
+        for (var i = 0, j = nickname.length; i < j; ++i) {
+            u16a[i] = nickname.charCodeAt(i);
+        }
+        let u8a = new Uint8Array(u16a.buffer);
+        Memory.RW.setValueAt(offset.address, u8a, offset.bits, this._bin);
+    }
 }
 
 module.exports = PKX;
