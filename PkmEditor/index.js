e938721c3c504f90b80d09f5e6652df13beaf983
DarkNik <mitakidisnikos@gmail.com>
2016-06-10 22:15:15 +0300

    refact varaibles name and add load SAV6 file

--- PkmEditor/index.js
+++ PkmEditor/index.js
@@ -1,17 +1,22 @@
 let fs = require('fs');
-let pkmObject = require('./Files');
+let SaveObj = require('./SaveObj');
 let data = require('./Data');
 
 let fileTypeSize = {
     GEN6_POKEMON_DECRYPDET: 0x104,
-    GEN6_SAVE_ORAS: 0x76000
+    GEN6_SAVE_ORAS: 0x76000,
+    GEN6_SAVE_XY: 0x65600
 };
 
 let loader = (path) => {
     let file = fs.readFileSync(path);
     switch (file.length) {
         case fileTypeSize.GEN6_POKEMON_DECRYPDET:
-            return new pkmObject.PK6(file);
+            return new SaveObj.PK6(file);
+        case fileTypeSize.GEN6_SAVE_ORAS:
+            return new SaveObj.PK6(file, 'ORAS');
+        case fileTypeSize.GEN6_SAVE_XY:
+            return new SaveObj.PK6(file, 'XY');
     }
 };
 
@@ -19,5 +24,5 @@ let loader = (path) => {
 module.exports = {
     load: loader,
     data: data,
-    pkmObject: pkmObject
+    SaveObj: SaveObj
 };
