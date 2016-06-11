91d19272ec2347c7a25781118776dd3f7f119455
DarkNik <mitakidisnikos@gmail.com>
2016-06-11 20:12:12 +0300

    get pokemon drom box on SAV6

--- PkmEditor/SaveObj/SAV6.js
+++ PkmEditor/SaveObj/SAV6.js
@@ -1,19 +1,30 @@
 let Memory = require('./memory');
-let Encryption = require('./encryption');
+let Encryption = require('./encryption/index');
+let PK6 = require('./PK6');
 
 class SAV6 {
     constructor(binary, type){
         this._bin = binary;
         this._type = type;
-        if(this._type == 'XY'){
-            this.offset = Memory.PK6.MAP.XY
-        }else if(this._type == 'ORAS'){
-            this.offset = Memory.PK6.MAP.ORAS
-        }
+        this._offset = Memory.SAV6.MAP[this._type];
     }
     
     get gameType() {
         return this._type;
+    }
+
+    getPkmFromBox(box, slot) {
+        let pos = (box * 30) + slot;
+        let pkxOffset = {
+            address: 0x33000 + (pos * 232),
+            bits: 232
+        };
+        let pkm = Memory.RW.getValueAt(pkxOffset,this._bin);
+        pkm = Encryption.PK6.decrypt(pkm);
+        if (Encryption.PK6.verifyChk(pkm) && (pkm[8] | pkm[9]) != 0) {
+            return new PK6(pkm);
+        }
+        return null;
     }
 }
 
